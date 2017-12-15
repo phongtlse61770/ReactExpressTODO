0df9b65e2bcbb5da3f32fa9d79089a5e178c44fd
Phong <tranloiphong2@gmail.com>
2017-12-15 19:41:25 +0700

    Create overall deploy script

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/app/index.html');
 });
 
 module.exports = router;
