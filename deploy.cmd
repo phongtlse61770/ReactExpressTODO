cacdca9a1d277a88999df2eb0cd68c5c8e39a10d
Phong <tranloiphong2@gmail.com>
2017-12-16 07:54:39 +0700

    install react-md

--- deploy.cmd
+++ deploy.cmd
@@ -1,11 +1,12 @@
 cd client
 call npm run build
 cd ..
-xcopy /s/e  "client/build" "react-backend/public/app"
+xcopy /s/e/y  "client/build" "react-backend/public/app"
 cd react-backend
 git add .
 git commit -am deploy
 git push heroku master
+start heroku open
 cd ..
 echo
 echo Complete !!
